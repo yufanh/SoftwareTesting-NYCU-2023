fa885d5c96305f70e7e89c83281ab4ee5b2cd278
Yu-Fan, Huang <82715170+yufanh@users.noreply.github.com>
2023-03-01 21:43:06 +0800

    Update StudentsTest.py

--- Lab01/StudentsTest.py
+++ Lab01/StudentsTest.py
@@ -14,12 +14,12 @@ class Test(unittest.TestCase):
             self.assertNotIn(id, self.user_id)
             self.user_id.append(id)
             print(id , name)
-        print("\nFinish set_name test\n")
+        print("\nFinish set_name test")
 
         
     def test_1_get_name(self):
         print("\n\nStart get_name test\n")
-        print("\nuser_id length = ", len(self.user_id))
+        print("user_id length = ", len(self.user_id))
         print("user_name length = ", len(self.user_id), "\n")
         for i in range(0, len(self.user_id)):
             name = self.students.get_name(self.user_id[i])
