cd5476616b9225bbf88f8fe57f3d72e740b41765
Yu-Fan, Huang <82715170+yufanh@users.noreply.github.com>
2023-03-04 14:39:00 +0800

    Update app_test.py

--- Lab02/app_test.py
+++ Lab02/app_test.py
@@ -9,21 +9,13 @@ class ApplicationTest(unittest.TestCase):
     selected_list = ["Willian", "Oilver", "Henry"]
 
     def setUp(self):
-
-        print("Start setUp\n")
-
         # stub
         mock_get_names = Mock()
         mock_get_names.return_value = (self.name_list, self.selected_list)
         app.Application.get_names = mock_get_names
         self.myapp = app.Application()
 
-        print("Finish setUp\n")
-
     def test_app(self):
-
-        print("Start test_app\n")
-
         # mock
         mock_get_random_person = Mock()
         mock_get_random_person.side_effect = self.name_list
@@ -33,27 +25,27 @@ class ApplicationTest(unittest.TestCase):
         print(selected, "selected")
 
         # spy
-        def fake_write(name):
-            context = 'Congrats, ' + name + '!'
-            return context
+        class fake_mail:
+            def fake_write(name):
+                context = 'Congrats, ' + name + '!'
+                return context
 
-        def fake_send(name, context):
-            print(context)
+            def fake_send(name, context):
+                print(context)
 
         mock_write = Mock()
-        mock_write.side_effect = fake_write
+        mock_write.side_effect = fake_mail.fake_write
         app.MailSystem.write = mock_write
 
         mock_send = Mock()
-        mock_send.side_effect = fake_send
+        mock_send.side_effect = fake_mail.fake_send
         app.MailSystem.send = mock_send
 
         self.myapp.notify_selected()
         self.assertEqual(mock_write.call_count, len(self.name_list))
         self.assertEqual(mock_send.call_count, len(self.name_list))
 
-        print("Finish test_app\n")
-
+        print("\n")
         print(mock_write.call_args_list)
         print(mock_send.call_args_list)
 
