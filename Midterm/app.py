d98e230ba0fd5ebc454b6ade15bb75d99714019f
Yu-Fan, Huang <yufanhuang88@gmail.com>
2023-04-13 15:26:16 +0800

    Create app.py

---
+++ Midterm/app.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import StaleElementReferenceException
+import time
+
+if __name__ == '__main__':
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options = options)
+    # driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver.maximize_window()
+    driver.get('https://docs.python.org/3/tutorial/index.html')
+    wait = WebDriverWait(driver, 10)
+    select = Select(wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]//*[@id="language_select"]'))))
+    select.select_by_value("zh-tw")
+
+    while True:
+        try:
+            print(selected_option = select.first_selected_option)
+        except StaleElementReferenceException:
+            break
+
+    myTitle = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]//h1')))
+    cont = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]//p')))
+    print(myTitle.text)
+    print(cont.text)
